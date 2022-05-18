ca81e05fb3629133296b29766fe608c169dfef60
jabber <2898117012@qq.com>
2022-05-18 23:21:11 +0800

    init

--- src/Http/Controllers/Admin/TrialController.php
+++ src/Http/Controllers/Admin/TrialController.php
@@ -2,12 +2,108 @@
 
 namespace Dealskoo\Trial\Http\Controllers\Admin;
 
+use Carbon\Carbon;
 use Dealskoo\Admin\Http\Controllers\Controller as AdminController;
+use Dealskoo\Admin\Rules\Slug;
+use Dealskoo\Trial\Models\Trial;
 use Illuminate\Http\Request;
 
 class TrialController extends AdminController
 {
     public function index(Request $request)
     {
+        abort_if(!$request->user()->canDo('trials.index'), 403);
+        if ($request->ajax()) {
+            return $this->table($request);
+        } else {
+            return view('trial::admin.trial.index');
+        }
+    }
+
+    private function table(Request $request)
+    {
+        $start = $request->input('start', 0);
+        $limit = $request->input('length', 10);
+        $keyword = $request->input('search.value');
+        $columns = ['id', 'title', 'refund', 'quantity', 'ship_fee', 'clicks', 'seller_id', 'product_id', 'category_id', 'country_id', 'brand_id', 'platform_id', 'approved_at', 'start_at', 'end_at', 'created_at', 'updated_at'];
+        $column = $columns[$request->input('order.0.column', 0)];
+        $desc = $request->input('order.0.dir', 'desc');
+        $query = Trial::query();
+        if ($keyword) {
+            $query->where('title', 'like', '%' . $keyword . '%');
+            $query->orWhere('slug', 'like', '%' . $keyword . '%');
+        }
+        $query->orderBy($column, $desc);
+        $count = $query->count();
+        $trials = $query->skip($start)->take($limit)->get();
+        $rows = [];
+        $can_view = $request->user()->canDo('trials.show');
+        $can_edit = $request->user()->canDo('trials.edit');
+        foreach ($trials as $trial) {
+            $row = [];
+            $row[] = $trial->id;
+            $row[] = $trial->title . ' <span class="badge bg-success">' . $trial->off . '% Off</span>';
+            $row[] = $trial->country->currency_symbol . $trial->refund . ' <del>' . $trial->country->currency_symbol . $trial->product->price . '</del>';
+            $row[] = $trial->quantity;
+            $row[] = $trial->country->currency_symbol . $trial->ship_fee;
+            $row[] = $trial->clicks;
+            $row[] = $trial->seller->name;
+            $row[] = $trial->product->name;
+            $row[] = $trial->category->name;
+            $row[] = $trial->country->name;
+            $row[] = $trial->brand ? $trial->brand->name : '';
+            $row[] = $trial->platform ? $trial->platform->name : '';
+            $row[] = $trial->approved_at != null ? Carbon::parse($trial->approved_at)->format('Y-m-d H:i:s') : null;
+            $row[] = $trial->start_at != null ? Carbon::parse($trial->start_at)->format('Y-m-d') : null;
+            $row[] = $trial->end_at != null ? Carbon::parse($trial->end_at)->format('Y-m-d') : null;
+            $row[] = Carbon::parse($trial->created_at)->format('Y-m-d H:i:s');
+            $row[] = Carbon::parse($trial->updated_at)->format('Y-m-d H:i:s');
+            $view_link = '';
+            if ($can_view) {
+                $view_link = '<a href="' . route('admin.trials.show', $trial) . '" class="action-icon"><i class="mdi mdi-eye"></i></a>';
+            }
+
+            $edit_link = '';
+            if ($can_edit) {
+                $edit_link = '<a href="' . route('admin.trials.edit', $trial) . '" class="action-icon"><i class="mdi mdi-square-edit-outline"></i></a>';
+            }
+            $row[] = $view_link . $edit_link;
+            $rows[] = $row;
+        }
+        return [
+            'draw' => $request->draw,
+            'recordsTotal' => $count,
+            'recordsFiltered' => $count,
+            'data' => $rows
+        ];
+    }
+
+    public function show(Request $request, $id)
+    {
+        abort_if(!$request->user()->canDo('trials.show'), 403);
+        $trial = Trial::query()->findOrFail($id);
+        return view('trial::admin.trial.show', ['trial' => $trial]);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        abort_if(!$request->user()->canDo('trials.edit'), 403);
+        $trial = Trial::query()->findOrFail($id);
+        return view('trial::admin.trial.edit', ['trial' => $trial]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        abort_if(!$request->user()->canDo('trials.edit'), 403);
+        $request->validate([
+            'slug' => ['required', new Slug('trials', 'slug', $id, 'id')]
+        ]);
+        $trial = Trial::query()->findOrFail($id);
+        $trial->fill($request->only([
+            'slug'
+        ]));
+        $trial->approved_at = $request->boolean('approved', false) ? Carbon::now() : null;
+        $trial->save();
+        return back()->with('success', __('admin::admin.update_success'));
     }
 }
--- src/Http/Controllers/Seller/TrialController.php
+++ src/Http/Controllers/Seller/TrialController.php
@@ -2,12 +2,138 @@
 
 namespace Dealskoo\Trial\Http\Controllers\Seller;
 
+use Carbon\Carbon;
+use Dealskoo\Product\Models\Product;
 use Dealskoo\Seller\Http\Controllers\Controller as SellerController;
+use Dealskoo\Trial\Models\Trial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class TrialController extends SellerController
 {
     public function index(Request $request)
     {
+        if ($request->ajax()) {
+            return $this->table($request);
+        } else {
+            return view('trial::seller.trial.index');
+        }
+    }
+
+    private function table(Request $request)
+    {
+        $start = $request->input('start', 0);
+        $limit = $request->input('length', 10);
+        $keyword = $request->input('search.value');
+        $columns = ['id', 'title', 'refund', 'quantity', 'ship_fee', 'clicks', 'product_id', 'category_id', 'country_id', 'brand_id', 'platform_id', 'approved_at', 'start_at', 'end_at', 'created_at', 'updated_at'];
+        $column = $columns[$request->input('order.0.column', 0)];
+        $desc = $request->input('order.0.dir', 'desc');
+        $query = Trial::query();
+        if ($keyword) {
+            $query->where('title', 'like', '%' . $keyword . '%');
+            $query->orWhere('slug', 'like', '%' . $keyword . '%');
+        }
+        $query->orderBy($column, $desc);
+        $count = $query->count();
+        $trials = $query->skip($start)->take($limit)->get();
+        $rows = [];
+        foreach ($trials as $trial) {
+            $row = [];
+            $row[] = $trial->id;
+            $row[] = $trial->title . ' <span class="badge bg-success">' . $trial->off . '% Off</span>';
+            $row[] = $trial->country->currency_symbol . $trial->price . ' <del>' . $trial->country->currency_symbol . $trial->product->price . '</del>';
+            $row[] = $trial->country->currency_symbol . $trial->ship_fee;
+            $row[] = $trial->clicks;
+            $row[] = $trial->code;
+            $row[] = $trial->product->name;
+            $row[] = $trial->category->name;
+            $row[] = $trial->country->name;
+            $row[] = $trial->brand ? $trial->brand->name : '';
+            $row[] = $trial->platform ? $trial->platform->name : '';
+            $row[] = $trial->approved_at != null ? Carbon::parse($trial->approved_at)->format('Y-m-d H:i:s') : null;
+            $row[] = $trial->start_at != null ? Carbon::parse($trial->start_at)->format('Y-m-d') : null;
+            $row[] = $trial->end_at != null ? Carbon::parse($trial->end_at)->format('Y-m-d') : null;
+            $row[] = Carbon::parse($trial->created_at)->format('Y-m-d H:i:s');
+            $row[] = Carbon::parse($trial->updated_at)->format('Y-m-d H:i:s');
+            $edit_link = '';
+            $destroy_link = '';
+            if ($trial->approved_at == null) {
+                $edit_link = '<a href="' . route('seller.trials.edit', $trial) . '" class="action-icon"><i class="mdi mdi-square-edit-outline"></i></a>';
+                $destroy_link = '<a href="javascript:void(0);" class="action-icon delete-btn" data-table="trials_table" data-url="' . route('seller.trials.destroy', $trial) . '"> <i class="mdi mdi-delete"></i></a>';
+            }
+            $row[] = $edit_link . $destroy_link;
+            $rows[] = $row;
+        }
+        return [
+            'draw' => $request->draw,
+            'recordsTotal' => $count,
+            'recordsFiltered' => $count,
+            'data' => $rows
+        ];
+    }
+
+    public function create(Request $request)
+    {
+        $products = Product::approved()->where('seller_id', $request->user()->id)->get();
+        return view('trial::seller.trial.create', ['products' => $products]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', 'string'],
+            'product_id' => ['required', 'exists:products,id'],
+            'refund' => ['required', 'numeric'],
+            'quantity' => ['required', 'numeric'],
+            'ship_fee' => ['required', 'numeric'],
+            'activity_date' => ['required', 'string']
+        ]);
+        $between = explode(' - ', $request->input('activity_date'));
+        $start = date('Y-m-d', strtotime($between[0]));
+        $end = date('Y-m-d', strtotime($between[1]));
+        $product = Product::approved()->where('seller_id', $request->user()->id)->findOrFail($request->input('product_id'));
+        $trial = new Trial(Arr::collapse([$request->only([
+            'title', 'product_id', 'refund', 'quantity', 'ship_fee'
+        ]), $product->only([
+            'seller_id', 'category_id', 'country_id', 'brand_id', 'platform_id'
+        ]), ['start_at' => $start, 'end_at' => $end]]));
+        $trial->save();
+        return redirect(route('seller.trials.index'));
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $trial = Trial::where('seller_id', $request->user()->id)->findOrFail($id);
+        $products = Product::approved()->where('seller_id', $request->user()->id)->get();
+        return view('trial::seller.trial.edit', ['trial' => $trial, 'products' => $products]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => ['required', 'string'],
+            'product_id' => ['required', 'exists:products,id'],
+            'refund' => ['required', 'numeric'],
+            'quantity' => ['required', 'numeric'],
+            'ship_fee' => ['required', 'numeric'],
+            'activity_date' => ['required', 'string']
+        ]);
+        $between = explode(' - ', $request->input('activity_date'));
+        $start = date('Y-m-d', strtotime($between[0]));
+        $end = date('Y-m-d', strtotime($between[1]));
+        $product = Product::approved()->where('seller_id', $request->user()->id)->findOrFail($request->input('product_id'));
+        $trial = Trial::where('seller_id', $request->user()->id)->findOrFail($id);
+        $trial->fill(Arr::collapse([$request->only([
+            'title', 'product_id', 'refund', 'quantity', 'ship_fee'
+        ]), $product->only([
+            'seller_id', 'category_id', 'country_id', 'brand_id', 'platform_id'
+        ]), ['start_at' => $start, 'end_at' => $end]]));
+        $trial->save();
+        return redirect(route('seller.trials.index'));
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        return ['status' => Trial::where('seller_id', $request->user()->id)->where('approved_at', null)->where('id', $id)->delete()];
     }
 }
