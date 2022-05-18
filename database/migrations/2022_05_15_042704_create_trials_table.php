1480875f7ed80abfad88e1b7f09a93375beef104
jabber <2898117012@qq.com>
2022-05-18 21:25:19 +0800

    init

--- database/migrations/2022_05_15_042704_create_trials_table.php
+++ database/migrations/2022_05_15_042704_create_trials_table.php
@@ -15,9 +15,28 @@ class CreateTrialsTable extends Migration
     {
         Schema::create('trials', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique()->nullable();
+            $table->decimal('refund', 10, 2)->default(0);
+            $table->decimal('ship_fee', 10, 2)->default(0);
             $table->integer('clicks')->default(0);
+            $table->unsignedBigInteger('seller_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->unsignedBigInteger('platform_id')->nullable();
+            $table->timestamp('approved_at')->nullable();
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
             $table->timestamps();
             $table->softDeletes();
+            $table->foreign('seller_id')->references('id')->on('sellers')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('country_id')->references('id')->on('countries')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('platform_id')->references('id')->on('platforms')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
