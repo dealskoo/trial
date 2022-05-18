fdb61f63b63e4b33429c8ac84ab0067a2578ed01
jabber <2898117012@qq.com>
2022-05-18 23:03:40 +0800

    init

--- database/factories/TrialFactory.php
+++ database/factories/TrialFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories\Dealskoo\Trial\Models;
 
+use Dealskoo\Product\Models\Product;
 use Dealskoo\Trial\Models\Trial;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -21,8 +22,41 @@ class TrialFactory extends Factory
      */
     public function definition()
     {
+        $product = Product::factory()->approved()->create();
         return [
-            //
+            'title' => $this->faker->title,
+            'slug' => $this->faker->slug,
+            'refund' => $this->faker->numberBetween(0, 100),
+            'quantity' => $this->faker->numberBetween(0, 100),
+            'ship_fee' => $this->faker->numberBetween(0, 100),
+            'seller_id' => $product->seller_id,
+            'product_id' => $product->id,
+            'category_id' => $product->category_id,
+            'country_id' => $product->country_id,
+            'brand_id' => $product->brand_id,
+            'platform_id' => $product->platform_id,
+            'start_at' => $this->faker->dateTime,
+            'end_at' => $this->faker->dateTime
         ];
+    }
+
+    public function approved()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'approved_at' => $this->faker->dateTime,
+            ];
+        });
+    }
+
+    public function avaiabled()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'start_at' => $this->faker->dateTimeBetween('-1 days'),
+                'end_at' => $this->faker->dateTimeBetween('now', '+7 days'),
+                'approved_at' => $this->faker->dateTime,
+            ];
+        });
     }
 }
--- src/Models/Trial.php
+++ src/Models/Trial.php
@@ -2,11 +2,69 @@
 
 namespace Dealskoo\Trial\Models;
 
+use Carbon\Carbon;
+use Dealskoo\Admin\Traits\HasSlug;
+use Dealskoo\Brand\Traits\HasBrand;
+use Dealskoo\Category\Traits\HasCategory;
+use Dealskoo\Country\Traits\HasCountry;
+use Dealskoo\Platform\Traits\HasPlatform;
+use Dealskoo\Product\Traits\HasProduct;
+use Dealskoo\Seller\Traits\HasSeller;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Trial extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasSlug, HasCategory, HasCountry, HasSeller, HasBrand, HasPlatform, HasProduct;
+
+    protected $appends = [
+        'cover', 'cover_url'
+    ];
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'refund',
+        'quantity',
+        'ship_fee',
+        'clicks',
+        'seller_id',
+        'product_id',
+        'category_id',
+        'country_id',
+        'brand_id',
+        'platform_id',
+        'approved_at',
+        'start_at',
+        'end_at'
+    ];
+
+    protected $casts = [
+        'approved_at' => 'datetime',
+        'start_at' => 'datetime',
+        'end_at' => 'datetime'
+    ];
+
+    public function getCoverAttribute()
+    {
+        return $this->product->cover;
+    }
+
+    public function getCoverUrlAttribute()
+    {
+        return $this->product->cover_url;
+    }
+
+    public function scopeApproved(Builder $builder)
+    {
+        return $builder->whereNotNull('approved_at');
+    }
+
+    public function scopeAvaiabled(Builder $builder)
+    {
+        $now = Carbon::now();
+        return $builder->whereNotNull('approved_at')->where('start_at', '<=', $now)->where('end_at', '>=', $now);
+    }
 }
